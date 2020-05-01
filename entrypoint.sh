45a8d92516864415a6abff7c56cbac6beb5abc5f
seyvoue <seyvoue@qq.com>
2020-05-01 15:50:02 +0800

    feat: initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+CONFIG_DIR=${CONFIG_DIR:-"/config"}
+SS_CONFIG=${SS_CONFIG:-""}
+SS_MODULE=${SS_MODULE:-"ss-server"}
+KCP_CONFIG=${KCP_CONFIG:-""}
+KCP_MODULE=${KCP_MODULE:-"kcpserver"}
+KCP_FLAG=${KCP_FLAG:-"false"}
+
+while getopts "s:m:k:e:x" OPT; do
+    case $OPT in
+        s)
+            SS_CONFIG=$OPTARG;;
+        m)
+            SS_MODULE=$OPTARG;;
+        k)
+            KCP_CONFIG=$OPTARG;;
+        e)
+            KCP_MODULE=$OPTARG;;
+        x)
+            KCP_FLAG="true";;
+    esac
+done
+
+export CONFIG_DIR=${CONFIG_DIR}
+export SS_CONFIG=${SS_CONFIG}
+export SS_MODULE=${SS_MODULE}
+export KCP_CONFIG=${KCP_CONFIG}
+export KCP_MODULE=${KCP_MODULE}
+export KCP_FLAG=${KCP_FLAG}
+
+exec runsvdir -P /etc/service
+goproxy
